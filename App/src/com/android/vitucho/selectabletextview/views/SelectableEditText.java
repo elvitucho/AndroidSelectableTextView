856e0ba39f603475d1e5355e405f31b4adfeb4f8
elvitucho <el_vitucho@yahoo.com>
2013-10-21 23:49:25 -0700

    selectableTextView and SelectableEditText created

---
+++ App/src/com/android/vitucho/selectabletextview/views/SelectableEditText.java
@@ -0,0 +1,63 @@
+package com.android.vitucho.selectabletextview.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ActionMode;
+import android.view.ActionMode.Callback;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.android.vitucho.selectabletextview.R;
+
+public class SelectableEditText extends EditText {
+
+	public SelectableEditText(Context context) {
+		super(context);
+		init();
+	}
+
+	public SelectableEditText(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init();
+	}
+
+	public SelectableEditText(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	private void init() {
+		this.setTextIsSelectable(true);
+		this.setCustomSelectionActionModeCallback(new Callback() {
+			
+			@Override
+			public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+				// Inflate a menu resource providing context menu items
+				MenuInflater inflater = mode.getMenuInflater();
+				// Assumes that you have "contexual.xml" menu resources
+				inflater.inflate(R.menu.contextual, menu);
+				return true;
+			}
+			
+			@Override
+			public void onDestroyActionMode(ActionMode mode) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		});
+	}
+}
---
+++ App/src/com/android/vitucho/selectabletextview/views/SelectableTextView.java
@@ -0,0 +1,12 @@
+package com.android.vitucho.selectabletextview.views;
+
+import android.content.Context;
+import android.widget.TextView;
+
+public class SelectableTextView extends TextView {
+
+	public SelectableTextView(Context context) {
+		super(context);
+	}
+
+}
